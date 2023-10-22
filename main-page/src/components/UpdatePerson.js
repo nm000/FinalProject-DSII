e033e72ff840adca83cfe820a41caa10383c1134
MaycolMD <maycolm@uninorte.edu.co>
2023-10-22 11:56:45 -0500

    First validations

--- main-page/src/components/UpdatePerson.js
+++ main-page/src/components/UpdatePerson.js
@@ -1,4 +1,45 @@
 import React, { useState , useEffect } from 'react';
+import {useNavigate} from 'react-router-dom';
+
+function validarFormatoFecha(fecha) {
+  // Define una expresión regular para el formato "dd-mm-aaaa"
+  const formatoFecha = /^(\d{2})-(\d{2})-(\d{4})$/;
+  
+  // Comprueba si la fecha coincide con el formato
+  if (!formatoFecha.test(fecha)) {
+    return false; // La fecha no cumple con el formato
+  }
+  
+  // Extrae los componentes de la fecha
+  const [, dia, mes, anio] = fecha.match(formatoFecha);
+
+  // Convierte los componentes a números enteros
+  const diaNumero = parseInt(dia, 10);
+  const mesNumero = parseInt(mes, 10);
+  const anioNumero = parseInt(anio, 10);
+
+  // Valida que los componentes sean válidos
+  if (
+    diaNumero < 1 || diaNumero > 31 ||
+    mesNumero < 1 || mesNumero > 12 ||
+    anioNumero < 1900 // Puedes ajustar el año mínimo según tus necesidades
+  ) {
+    return false; // La fecha no es válida
+  }
+
+  // Puedes realizar validaciones adicionales según tus requisitos
+
+  // La fecha cumple con el formato y es válida
+  return true;
+}
+
+function validarCorreoElectronico(correo) {
+  // Define una expresión regular para validar el formato de correo electrónico
+  const formatoCorreo = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+  
+  // Comprueba si el correo electrónico coincide con el formato
+  return formatoCorreo.test(correo);
+}
 
 export const UpdatePerson = () => {
 
@@ -16,7 +57,7 @@ export const UpdatePerson = () => {
 
     // Inicializar variable que contendrá todos los datos de la persona
     const [personaData, setPersonaData] = useState({});
-
+    const navigate = useNavigate();
     // Obtener la URL de la página
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
@@ -71,22 +112,67 @@ export const UpdatePerson = () => {
         e.preventDefault();
         
         // Envío del método PUT para realizar el UPDATE
-        await fetch(`http://localhost:8003/${datos}`, {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                tipoDocumento,
-                primerNombre,
-                segundoNombre,
-                apellidos,
-                fechaNacimiento,
-                genero,
-                correoElectronico,
-                celular,
-                foto // Aquí va la foto (bytes)
-            }),
-        });
-        
+        // Envío del método POST para realizar el CREATE con los datos
+    if (tipoDocumento === 'Tarjeta de identidad' || tipoDocumento === 'Cedula'){
+      if (typeof numDocumento === "number" && numDocumento.length <= 10){
+        if (typeof primerNombre != "number" && primerNombre.length <= 30){
+          if (typeof segundoNombre != "number" && segundoNombre.length <= 30){
+            if (typeof apellidos != "number" && apellidos.length <= 60){
+              if (validarFormatoFecha(fechaNacimiento)){
+                if (genero === 'Masculino' || genero === 'Femenino' || genero === 'No binario' || genero === 'Prefiero no responder') {
+                  if (validarCorreoElectronico(correoElectronico)){
+                    if (typeof celular === "number" && celular.length === 10){
+                      
+                      // falta la foto
+                      await fetch(`http://localhost:8003/${datos}`, {
+                        method: 'PUT',
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify({
+                            tipoDocumento,
+                            primerNombre,
+                            segundoNombre,
+                            apellidos,
+                            fechaNacimiento,
+                            genero,
+                            correoElectronico,
+                            celular,
+                            foto // Aquí va la foto (bytes)
+                        }),
+                    });
+
+                      // Esto lo único que hace es que retrocede uno a la página
+                      // Por ejemplo cuando el create se hace correctamente, te devuelve a la página del menú de opciones  
+                      await navigate(-1);
+
+
+                    } else {
+                      // celular invalido
+                    }
+                  } else {
+                    // correo no valido
+                  }
+                } else {
+                  // genero no valido
+                }
+              } else {
+                // formato invalido (debe ser dd-mm-aaaa)
+              }
+            } else {
+              console.log('Digite apellidos sin números y menos de 60 caracteres')
+            }
+          } else {
+            console.log('Digite un segundo nombre sin número y de menos de 30 caracteres')
+          }
+        } else {
+          console.log('Digite un nombre sin número y de menos de 30 caracteres')
+        }
+      } else {
+        console.log('digite un número no mayor de 10 caracteres')
+      }
+    } else {
+      console.log('digite tipo de doc valido')
+    }
+            
     }
     
     // HTML DEL CÓDIGO
@@ -94,15 +180,16 @@ export const UpdatePerson = () => {
     <form>
     <div>
       <label htmlFor="tipoDocumento">Tipo de Documento:</label>
-      <input
-        type="text"
+      <select
         id="tipoDocumento"
         name="tipoDocumento"
-        // El valor default será el actual de la persona
-        defaultValue={personaData.tipoDocumento}
-        // onChange actualiza la variable del dato cada vez que se cambia en el input
         onChange={e => setTipoDocumento(e.target.value)}
-      />
+      >
+        <option value="">Selecciona una opción</option>
+        <option value="Tarjeta de identidad">Tarjeta de identidad</option>
+        <option value="Cedula">Cédula</option>
+      </select>
+
     </div>
     <div>
       <label htmlFor="nroDocumento">Nro. Documento:</label>
@@ -151,6 +238,8 @@ export const UpdatePerson = () => {
         type="date"
         id="fechaNacimiento"
         name="fechaNacimiento"
+        pattern="\d{2}-\d{2}-\d{4}"
+        placeholder="dd-mm-aaaa"
         defaultValue={personaData.fechaNacimiento}
         onChange={e => setFechaNacimiento(e.target.value)}
       />
@@ -166,6 +255,8 @@ export const UpdatePerson = () => {
         <option value="">Selecciona una opción</option>
         <option value="Masculino">Masculino</option>
         <option value="Femenino">Femenino</option>
+        <option value="No binario">No binario</option>
+        <option value="Prefiero no responder">Prefiero no responder</option>
       </select>
     </div>
     <div>
@@ -205,4 +296,4 @@ export const UpdatePerson = () => {
 
     </form>
   </div>
-}
+} 
