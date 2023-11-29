9982bd7ef173e9c072a267bca9e418e6e6a17243
nm000 <mendozanatalia0827@gmail.com>
2023-11-28 20:57:20 -0500

    try .env

--- MS-Create/main.py
+++ MS-Create/main.py
@@ -15,6 +15,7 @@ from pydantic import BaseModel  # Importa BaseModel de Pydantic
 from datetime import datetime
 import base64
 import pytz
+import os
 
 app = FastAPI()
 
@@ -31,8 +32,10 @@ app.add_middleware(
 #SQLALCHEMY_DATABASE_URL = "mssql+pyodbc://DESKTOP-61S4LKS\SQLEXPRESS/AppPersonas?driver=ODBC+driver+17+for+SQL+Server"
 #DESKTOP-61S4LKS
 #DESKTOP-T1KMRV2
-
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://user:123456@db:3306/apppersonas"
+user = os.getenv("DB_USER")
+psw = os.getenv("DB_PW")
+name = os.getenv("DB_NAME")
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{user}:{psw}@db:3306/{name}"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
